9980e40e7d050f5a502f1ef92f658533ad5201b6
GithubActions <actions@github.com>
2024-12-12 08:08:28 +0530

    -Adding debug print to check for error

--- sherpa_ort/core.py
+++ sherpa_ort/core.py
@@ -6,7 +6,7 @@ import sherpa_onnx
 import wave
 from .utils import get_current_dir
 
-def generate_audio(text):
+def generate_audio(text: str, sid: int = 0, speed: float = 1.0, max_num_sentences: int = -1, num_threads: int = 4):
     """
     Generate audio for a given text using the Sherpa-ONNX Offline TTS engine.
 
@@ -17,6 +17,7 @@ def generate_audio(text):
     - bytes: The generated audio in WAV format as an in-memory binary file.
     """
     current_dir = get_current_dir()
+    print(f"Current Dir: {current_dir}")
     args = {
         "vits_model": str(os.path.join(current_dir, 'assets', 'en_US-libritts_r-medium.ort')),
         "vits_lexicon": "",
@@ -24,13 +25,15 @@ def generate_audio(text):
         "vits_data_dir": str(os.path.join(current_dir, 'assets', 'espeak-ng-data')),
         "vits_dict_dir": "",
         "tts_rule_fsts": "",
-        "max_num_sentences": 100,
-        "sid": 0,
+        "max_num_sentences": max_num_sentences,
+        "sid": sid,
         "debug": False,
-        "provider": "cuda",
-        "num_threads": 4,
-        "speed": 1.0,
+        "provider": "cpu",
+        "num_threads": num_threads,
+        "speed": speed,
     }
+    
+    print(f"Args: {args}")
 
     # Configure the TTS engine
     tts_config = sherpa_onnx.OfflineTtsConfig(
@@ -49,13 +52,19 @@ def generate_audio(text):
         rule_fsts=args["tts_rule_fsts"],
         max_num_sentences=args["max_num_sentences"],
     )
+    
+    print(f"Validating Args")
 
     # Validate configuration
     if not tts_config.validate():
         raise ValueError("Invalid TTS configuration. Please check your config.")
 
+    print(f"Initialization of TTS")
+
     # Initialize the TTS engine
     tts = sherpa_onnx.OfflineTts(tts_config)
+    
+    print(f"Generating the Audio")
 
     # Generate audio
     start = time.time()
@@ -66,6 +75,8 @@ def generate_audio(text):
     )
     end = time.time()
 
+    print(f"Audio has been generated: {len(audio.samples)}")
+
     # Check if audio was generated successfully
     if len(audio.samples) == 0:
         raise RuntimeError("Error in generating audio. Please check your configuration.")
@@ -74,7 +85,10 @@ def generate_audio(text):
     elapsed_seconds = end - start
     audio_duration = len(audio.samples) / audio.sample_rate
     real_time_factor = elapsed_seconds / audio_duration
-
+    print(f"Elapsed Time: {elapsed_seconds:.2f} seconds")
+    print(f"Audio Duration: {audio_duration:.2f} seconds")
+    print(f"Real-Time Factor: {real_time_factor:.2f}")
+    
     # Return the in-memory WAV file as bytes
     return [audio, elapsed_seconds, audio_duration, real_time_factor]
 
